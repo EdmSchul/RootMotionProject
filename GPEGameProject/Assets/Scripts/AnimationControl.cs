5208ddd231aecb24903e48cd5a28172a62e49e0f
Edmund Carl Schultz III <edmschul@uat.edu>
2020-04-20 12:15:34 -0700

    First Push
    The first push of all files. Includes in-progress Milestone 2

---
+++ GPEGameProject/Assets/Scripts/AnimationControl.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationControl : MonoBehaviour {
+
+    private float speed = 4f;
+
+    private Animator animator;
+
+    Vector3 LookTarget;
+
+    float WarpTimer = 10;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	private void Update () {
+        
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        input = Vector3.ClampMagnitude(input, 1);
+        input *= speed;
+        animator.SetFloat("Horizontal", input.x);
+        animator.SetFloat("Vertical", input.z);
+
+        LookToPoint();
+        WarpDash();
+        if (WarpTimer < 10) {
+            WarpTimer += Time.deltaTime;
+        }
+        print(WarpTimer);
+    }
+
+    void LookToPoint() {
+        Ray LookToRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit LookHit;
+        if (Physics.Raycast(LookToRay, out LookHit)) {
+            LookTarget = LookHit.point;
+        } 
+        transform.LookAt(LookTarget);
+    }
+
+    void WarpDash() {
+        if (WarpTimer >= 10) 
+        {
+            if (transform.position.x < 85 && transform.position.x > 5)
+            {
+                if (transform.position.z < 85 && transform.position.z > 5)
+                {
+                    if (Input.GetKey(KeyCode.Space))
+                    {
+                        transform.position += transform.forward * 5;
+                        WarpTimer = 0;
+                    }
+                }
+            }
+        }
+        
+    }
+}
---
+++ GPEGameProject/Assets/Scripts/CameraBoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBoom : MonoBehaviour
+{
+    // What the camera follows
+    public GameObject Player;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // grabs the player object from the scene
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // where to move the camera in terms of x
+        float MovingXPos = Player.transform.position.x;
+
+        // ditto in z
+        float MovingZPos = Player.transform.position.z - 1f;
+
+        // put them together for the result: moving the camera on a single plane with the tank
+        transform.position = new Vector3(MovingXPos, transform.position.y, MovingZPos);
+    }
+}
